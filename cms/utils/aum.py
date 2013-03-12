b3c6b84bb9a7cf2a5ef2bf5af909af2c1d8220b2
Ionel Maries Cristian <contact@ionelmc.ro>
2013-03-12 18:54:53 +0200

    Add cms.utils.aum.get_auth_model_name

--- cms/utils/aum.py
+++ cms/utils/aum.py
@@ -1,6 +1,9 @@
 import django
+from django.conf import settings
 if django.VERSION >= (1, 5):
     from django.contrib.auth import get_user_model #pylint: disable=W0611
+    get_auth_model_name = lambda: settings.AUTH_USER_MODEL
 else:
     from django.contrib.auth.models import User
     get_user_model = lambda: User
+    get_auth_model_name = lambda: 'auth.User'
