9b48ec63a12dd196a1612a0eca8a01aa1bf1bd1e
Ionel Maries Cristian <contact@ionelmc.ro>
2013-03-12 17:59:16 +0200

    Update version requirement for django to >=1.3.1,<1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 import cms
 
-        
+
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Web Environment',
@@ -28,7 +28,7 @@ setup(
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     install_requires=[
-        'Django>=1.3.1,<1.5',
+        'Django>=1.3.1,<1.6',
         'django-classy-tags>=0.3.4.1',
         'south>=0.7.2',
         'html5lib',
