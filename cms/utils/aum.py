d97160c335d25862815b18c0d0b38ee74e9c5f3b
Ionel Maries Cristian <contact@ionelmc.ro>
2013-03-12 18:46:37 +0200

    Oooop, I replaced too much stuff :)

--- cms/utils/aum.py
+++ cms/utils/aum.py
@@ -2,5 +2,5 @@ import django
 if django.VERSION >= (1, 5):
     from django.contrib.auth import get_user_model #pylint: disable=W0611
 else:
-    from cms.utils.aum import get_user_model; User=get_user_model()
+    from django.contrib.auth.models import User
     get_user_model = lambda: User
